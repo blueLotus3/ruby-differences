359fa2ba2cede31f93601a576695b8ec58a34607
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-02-17 17:15:54 -0800

    got genders to print

--- ruby_algos.rb
+++ ruby_algos.rb
@@ -9,6 +9,60 @@ Constitution for the United States of America)
 def longest(words)
  words.max_by(&:length)
 end
-puts longest(words)
+# puts longest(words) # => Constitution
 
   
+def transform(words)
+    words.map(&:capitalize)
+  end
+#   puts transform(words) # => transforms first letter of words to capital
+
+  # NUMBERS ARRAYS
+
+even_nums = [0, 2, 4, 6]
+odd_nums = [1, 5, 7, 9]
+mixed_nums = [0, 2, 4, 7]
+
+# def some_odd(numbers)
+#    numbers.any? { |number| number.odd?}
+# end
+
+# puts some_odd(odd_nums) # => true
+
+# def every_even(numbers)
+#     numbers.any? { |number| number.even?}
+#   end
+#   puts every_even(even_nums) # => true
+
+
+#   def sum_evens(numbers)
+#     numbers.reduce(:+)
+#   end
+#   puts sum_evens(even_nums) # => 12
+
+# STUDENTS ARRAY
+students = [
+    {name: 'John', grade: 8, gender: 'M'},
+    {name: 'Sarah', grade: 12, gender: 'F'},
+    {name: 'Bob', grade: 16, gender: 'M'},
+    {name: 'Johnny', grade: 2, gender: 'M'},
+    {name: 'Ethan', grade: 4, gender: 'M'},
+    {name: 'Paula', grade: 8, gender: 'F'},
+    {name: 'Donald', grade: 5, gender: 'M'},
+    {name: 'Jennifer', grade: 13, gender: 'F'},
+    {name: 'Courtney', grade: 15, gender: 'F'},
+    {name: 'Jane', grade: 9, gender: 'F'}
+]
+
+# MALE GENDER
+def is_male(students)
+ students.find_all { |student| student[:gender] == 'M'}
+end
+puts is_male(students)
+
+# FEMALE GENDER
+def is_male(students)
+    students.find_all { |student| student[:gender] == 'F'}
+   end
+   puts is_male(students)
+
