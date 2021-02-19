f9afa9ae8838e162a6b6115e2b2cae1b494b3f0a
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-02-18 16:57:03 -0800

    got some labels for instances

--- dojo_fighter.rb
+++ dojo_fighter.rb
@@ -2,43 +2,65 @@
 
 class Fighter 
 
-def initialize(strength,defense,luck,life)
+def initialize(name,strength,defense,luck,life)
+    @name = name 
     @defense = defense
     @strength = strength
     @luck = luck
     @life = life
 end
+def show_name
+    p name:@name
+end
 
 def show_defense
-    p @defense 
+    p defense:@defense 
 end
 
 def show_strength
-    p @strength 
+    p strength:@strength 
 end
 def show_luck
-   p @luck
+   p luck:@luck
 end
 
 def show_life 
-   p @life
+   p life:@life
 end
 
 def power_move
     puts "Hadoken"
 end
+
+def attack
+    @attack -= @life
+end
 end
 
-class Akuma < Fighter
+class Akuma< Fighter
 
 end
 
 class Ryu < Fighter
 
 end
-fighter = Fighter.new(10,10,10,10)
-fighter.show_defense
-fighter.show_strength
-fighter.show_life
-fighter.show_luck
-fighter.power_move
+fighter = Fighter.new("name",10,10,10,10)
+# fighter.show_defense
+# fighter.show_strength
+# fighter.show_life
+# fighter.show_luck
+# fighter.power_move
+
+akuma = Fighter.new("akuma",10,10,10,10)
+akuma.show_name
+akuma.show_defense
+akuma.show_life
+akuma.show_luck
+akuma.show_strength
+
+ryu = Fighter.new("ryu",10,10,10,10)
+ryu.show_name
+ryu.show_defense
+ryu.show_life
+ryu.show_luck
+ryu.show_strength
