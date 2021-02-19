0243b65475fd74a66d10d31a648d992f52be3128
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-02-18 18:18:12 -0800

    got dojo set up for fighters

--- dojo_fighter.rb
+++ dojo_fighter.rb
@@ -1,6 +1,9 @@
 # DOJO FIGHTER
 
+# FIGHTER CLASS
 class Fighter 
+    attr_reader :name
+    attr_accessor  :strength, :defense, :luck, :life
 
 def initialize(name,strength,defense,luck,life)
     @name = name 
@@ -32,35 +35,68 @@ def power_move
     puts "Hadoken"
 end
 
-def attack
-    @attack -= @life
-end
-end
+def attack(life)
+    @strength = strength
+     damage =  @strength -= @life
+        end
+ end
 
-class Akuma< Fighter
+class Akuma < Fighter
 
 end
 
 class Ryu < Fighter
 
 end
-fighter = Fighter.new("name",10,10,10,10)
+    class Dojo
+        def self.lift_weights(fighter)
+            fighter.strength += 1
+            puts "You are feeling your strength surge"
+        end
+        def self.endurance_training(fighter)
+            fighter.defense += 1
+            puts "Your blocking is unbeatable"
+        end
+        def self.coin_in_fountain(fighter)
+            fighter.strength += 1
+            puts "You are feeling your strength surge"
+        end
+        
+    end
+
+
+fighter = Fighter.new("name",10,20,10,10)
 # fighter.show_defense
 # fighter.show_strength
 # fighter.show_life
 # fighter.show_luck
 # fighter.power_move
 
-akuma = Fighter.new("akuma",10,10,10,10)
-akuma.show_name
-akuma.show_defense
-akuma.show_life
-akuma.show_luck
-akuma.show_strength
+akuma = Fighter.new("akuma",10,20,10,10)
+# akuma.show_name
+# akuma.show_defense
+# akuma.show_life
+# akuma.show_luck
+# akuma.show_strength
 
 ryu = Fighter.new("ryu",10,10,10,10)
-ryu.show_name
-ryu.show_defense
-ryu.show_life
-ryu.show_luck
+# ryu.show_name
+# ryu.show_defense
+# ryu.show_life
+# ryu.show_luck
+# ryu.show_strength
+
+# ryu.show_life
+# ryu.attack
+# akuma.show_life
 ryu.show_strength
+Dojo.lift_weights(ryu)
+ryu.strength
+
+
+
+
+
+
+
+
